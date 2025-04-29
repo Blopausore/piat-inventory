dc1592c8a0b55bdb1b4074503fd6332dd193a86c
Blopausore <augustin_650@hotmail.fr>
2025-04-29 18:00:31 +0700

    Cleaning

--- core/views/client_order/__init__.py
+++
@@ -1,4 +0,0 @@
-from .list import supplier_orders_list, supplier_orders_import_page
-from .api import supplier_orders_json, supplier_order_update
-from .export import supplier_orders_export
-from .import_ import supplier_orders_import_upload
